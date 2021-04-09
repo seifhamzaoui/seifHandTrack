791df4308a313ca362878eadd7531cde76c4cac4
seifhamzaoui <64955781+seifhamzaoui@users.noreply.github.com>
2021-04-09 14:40:34 -0700

    Add files via upload

--- app.js
+++ app.js
@@ -7,30 +7,34 @@ let ctx = canvas.getContext("2d");
 let model;
 handTrack.startVideo(video).then(function (status) {
   if (status) {
-    navigator.getUserMedia({ video: {} }, function (stream) {
-      video.srcObject = stream;
-      setInterval(run, 1000);
-    },function(err){});
   }
 });
 
 function run() {
   model.detect(video).then(function (pred) {
     console.log(pred);
-    if(pred.length > 0){
-         audio.play()
+    if (pred.length > 0) {
+      audio.play();
     }
   });
 }
 
 handTrack
   .load({
-     flipHorizontal: true,   // flip e.g for video 
-     imageScaleFactor: 0.7,  // reduce input image size for gains in speed.
-     maxNumBoxes: 20,        // maximum number of boxes to detect
-     iouThreshold: 0.5,      // ioU threshold for non-max suppression
-     scoreThreshold: 0.3,    // confidence threshold for predictions.
-   })
+    flipHorizontal: true, // flip e.g for video
+    imageScaleFactor: 0.7, // reduce input image size for gains in speed.
+    maxNumBoxes: 20, // maximum number of boxes to detect
+    iouThreshold: 0.5, // ioU threshold for non-max suppression
+    scoreThreshold: 0.3, // confidence threshold for predictions.
+  })
   .then(function (lmodel) {
     model = lmodel;
+    navigator.getUserMedia(
+      { video: {} },
+      function (stream) {
+        video.srcObject = stream;
+        setInterval(run, 1000);
+      },
+      function (err) {}
+    );
   });
