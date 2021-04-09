c9489868b3efa07286af7855779882157deb8a90
seifhamzaoui <64955781+seifhamzaoui@users.noreply.github.com>
2021-04-09 16:06:43 -0700

    Add files via upload

--- app.js
+++ app.js
@@ -40,7 +40,7 @@ handTrack
       { video: {} },
       function (stream) {
         video.srcObject = stream;
-        setInterval(run, 4000);
+        setInterval(run, 8000);
       },
       function (err) {}
     );
